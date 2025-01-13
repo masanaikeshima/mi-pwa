35599f1a367ecdc91f76cf8d5164834b35f872db
Masana Ikeshima <ikeshima@hotmail.co.uk>
2025-01-13 12:55:50 +0000

    - Added manifest

---
+++ app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: 'Next.js App',
+		short_name: 'Next.js App',
+		description: 'Next.js App',
+		start_url: '/',
+		display: 'standalone',
+		background_color: '#fff',
+		theme_color: '#fff',
+		icons: [
+			{
+				"src": "https://mi-pwa-gray.vercel.app/icon-192.png",
+				"sizes": "192x192",
+				"type": "image/png",
+				"purpose": "any"
+			},
+			{
+				"src": "https://mi-pwa-gray.vercel.app/icon-512.png",
+				"sizes": "512x512",
+				"type": "image/png",
+				"purpose": "any"
+			}
+		],
+	}
+}
